2a59349e2b3e401d824d399c42d996ac9a745b14
MarcoRirihena <marcoleoniriihena@gmail.com>
2025-10-25 15:52:00 +0900

    reset credentials

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -1,29 +1,29 @@
-<!-- <?php -->
+<?php
 
-// namespace Database\Seeders;
+namespace Database\Seeders;
 
-// use Illuminate\Database\Seeder;
-// use Illuminate\Support\Facades\Hash;
-// use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
-// class AdminSeeder extends Seeder
-// {
-//     public function run(): void
-//     {
-//         // Buat Admin
-//         User::create([
-//             'name' => 'Admin Mou Glowing',
-//             'email' => 'admin@mouglowing.com',
-//             'password' => Hash::make('admin123'),
-//             'role' => 'admin'
-//         ]);
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Buat Admin
+        User::create([
+            'name' => 'Admin Mou Glowing',
+            'email' => 'admin@mouglowing.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin'
+        ]);
 
-//         // Buat User biasa untuk testing
-//         User::create([
-//             'name' => 'Customer Test',
-//             'email' => 'user@test.com',
-//             'password' => Hash::make('user123'),
-//             'role' => 'user'
-//         ]);
-//     }
-// }
+        // Buat User biasa untuk testing
+        User::create([
+            'name' => 'Customer Test',
+            'email' => 'user@test.com',
+            'password' => Hash::make('user123'),
+            'role' => 'user'
+        ]);
+    }
+}
