e2e08a142b55942cc9dc4cdac64084505324e0e3
MarcoRirihena <marcoleoniriihena@gmail.com>
2025-10-26 03:40:40 +0900

    final touch

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,19 +11,19 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         // Buat Admin
-        User::create([
-            'name' => 'Admin Mou Glowing',
-            'email' => 'admin@mouglowing.com',
-            'password' => Hash::make('admin123'),
-            'role' => 'admin'
-        ]);
+        // User::create([
+        //     'name' => 'Admin Mou Glowing',
+        //     'email' => 'admin@mouglowing.com',
+        //     'password' => Hash::make('admin123'),
+        //     'role' => 'admin'
+        // ]);
 
         // Buat User biasa untuk testing
-        User::create([
-            'name' => 'Customer Test',
-            'email' => 'user@test.com',
-            'password' => Hash::make('user123'),
-            'role' => 'user'
-        ]);
+    //     User::create([
+    //         'name' => 'Customer Test',
+    //         'email' => 'user@test.com',
+    //         'password' => Hash::make('user123'),
+    //         'role' => 'user'
+    //     ]);
     }
 }
