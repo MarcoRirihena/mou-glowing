fc9e064f7fe6c2cd55045ab7167e8839420b04ee
MarcoRirihena <marcoleoniriihena@gmail.com>
2025-10-28 11:31:17 +0900

    bercanda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rere cantik</title>
+    <title>Mou Glowing 13 - Premium Skincare</title>
     <!-- SEO Meta Tags -->
     <meta name="description" content="Mou Glowing 13 - Toko skincare online terpercaya dengan produk original. Jual masker wajah, cream wajah, dan kosmetik berkualitas untuk kecantikan kulit Anda.">
     <meta name="keywords" content="skincare, kosmetik, masker wajah, cream wajah, kecantikan, perawatan kulit, Mou Glowing 13">
@@ -383,7 +383,7 @@
     <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
         <div class="container">
             <a class="navbar-brand" href="/">
-                <i class="fas fa-spa"></i> MOU GLOWING 13
+                <i class="fas fa-spa"></i> Rere cantik
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
