31f8ce86d4adafda173febac5b5234673aa1b9c2
MarcoRirihena <marcoleoniriihena@gmail.com>
2025-10-28 11:35:33 +0900

    hehehe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -393,12 +393,12 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#home">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#products">Products</a>
-                    </li>
+                                        </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#about">About</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#products">Products</a>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact">Contact</a>
                     </li>
